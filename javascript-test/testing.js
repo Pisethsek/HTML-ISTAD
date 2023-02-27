97e30c9e47392cc8f6ff1cca4dd03fd90f339240
Pisethsek <pisethsek9899@gmail.com>
2023-02-27 08:26:12 +0700

    day3

---
+++ javascript-test/testing.js
@@ -0,0 +1,43 @@
+let a = 20 
+let b= 30
+let c = 200
+var old = 200
+
+console.log(a+c+b);
+
+var name = "Sek Piseth"
+const name2 = 'Sek  pisal'
+let name3 = "Pa Pa"
+var y = 90
+let y1 = 900
+const y2 = 9000
+
+console.log(name);
+console.log(name2);
+console.log(name3);
+console.log(y);
+console.log(y1);
+console.log(y2);
+
+console.log("-------------------------");
+const n = "sek"
+const pa = 'kimva'
+console.log(`my name is ${n} ${pa}`);
+
+const dam = true
+console.log(typeof(dam));
+
+console.log("-------------------------");
+let result;
+
+result = '2'/'1';
+
+console.log(result);
+
+
+console.log("-------------------------");
+let y12 = "1234"
+
+let z12 = Number(y12)
+
+console.log(z12+3);
